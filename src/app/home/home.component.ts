ffa2fdcfeabd0b667e96d5e4bc2b8940f7a9041d
U-CHEATHAM\Jacob.Pugh <Jacob.Pugh@SHSrm200-desk1.cheatham.k12.tn.us>
2025-04-11 15:19:42 -0500

    Angular test app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.less'
+})
+export class HomeComponent {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocationList:HousingLocation[] =  [];
+
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.housingLocationList;
+  };
+
+}
